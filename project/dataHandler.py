66b32a0e523cf762f446cf98dfc2c6be5531713d
mattiapeiretti <mattia.peiretti@gmail.com>
2021-07-15 03:41:33 +0200

    🚀 Refactored from main to dataHandler

--- project/dataHandler.py
+++ project/dataHandler.py
@@ -1,24 +1,72 @@
 from mcd_interface import MCDInterface
 from settingsHandler import SettingsHandler
 import csv
+import os
+
+
+def add_value_to_dict(dict, lat, lon, value):
+    if lat in dict.keys():
+        dictdata = dict[lat]
+        dictdata[lon] = value
+        dict[lat] = dictdata
+    else:
+        dict[lat] = {lon:  value}
+    return dict
+
+def print_data(slon, lat, lon, value, lat_margins, lon_margins):
+    print(f"Solar Longitude: {slon} Total Work: {100*(slon/360)}%, Latitude: {lat} Total Work: {100*((lat+90)/(lat_margins[1]+90))}%, Longitude: {lon} Total Work: {100*(lon/lon_margins[1])}%, Current Value: {value}",  end='\r')
+        
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 class dataHandler():
-    def __init__(self, dir_path, settings_handler):
+    def __init__(self, dir_path, settings_handler, mcd_interface):
         self.dir_path = dir_path
-        self.setting_handler = settings_handler
+        self.settings_handler = settings_handler
+        self.mcd_interface = mcd_interface
+
+    def collect_data(self, slon, lat_margins, lon_margins, record_type):
+
+        data = {}
+
+        lat_step = self.settings_handler.get_setting("LAT_STEP")
+        lon_step = self.settings_handler.get_setting("LON_STEP")
+
+        for lat in range(lat_margins[0], lat_margins[1] + lat_step, lat_step):
+
+            # If currently checking the poles, we will assign the data of one point to the whole scale.
+            if lat == lat_margins[0] or lat == lat_margins[1]:
+
+                current_value = self.mcd_interface.do_query(slon, record_type, [lat, 180])
+
+                for lon in range(lon_margins[0], lon_margins[1] + lon_step, lon_step):
+                    data = add_value_to_dict(data, lat, lon, current_value)
+
+                  
+                    print_data(slon, lat, lon, current_value, lat_margins, lon_margins)
+                continue
+
+            for lon in range(lon_margins[0], lon_margins[1] + lon_step, lon_step):
+                current_value = self.mcd_interface.do_query(slon, record_type, [lat, lon])
+
+                data = add_value_to_dict(data, lat, lon, current_value)
+                print_data(slon, lat, lon, current_value, lat_margins, lon_margins)
+                
+        return data
+
 
     def write_database_slon_record(self, record_type, slon, db_dict):
-        padded_slon = str(slon).rjust(self.setting_handler.get_setting("RECORDS_FILENAME_NUMBERS_PADDING"), "0")
+        padded_slon = str(slon).rjust(self.settings_handler.get_setting("RECORDS_FILENAME_NUMBERS_PADDING"), "0")
         filename = f"MCDas_{record_type}_LS{padded_slon}"
 
-        with open(self.dir_path + filename + '.csv', mode='w') as csv_file:
+        with open(self.dir_path + filename + '.csv', mode='w', newline='') as csv_file:
             fieldnames = ['lat', 'lon', 'min_temp']
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
             writer.writeheader()
             for lat in db_dict:
                 for lon in db_dict[lat]:
-                    print(lat, lon, db_dict[lat][lon])
                     writer.writerow({'lat': lat, 'lon': lon, 'min_temp':db_dict[lat][lon]})
 
     
--- project/main.py
+++ project/main.py
@@ -5,49 +5,19 @@ from dataHandler import dataHandler
 LAT_MARGINS = [-90, 90] 
 LON_MARGINS = [0, 360] 
 
-data = {}
-
-def add_value_to_dict(dict, lat, lon, value):
-    if lat in dict.keys():
-        dictdata = dict[lat]
-        dictdata[lon] = value
-        dict[lat] = dictdata
-    else:
-        dict[lat] = {lon:  current_value}
-    return dict
 
 if __name__ == "__main__":
 
     settings_handler = SettingsHandler()
     mcd_interface = MCDInterface(settings_handler.get_setting("MCD_BASELINK"))
-    data_hander = dataHandler("../output/", settings_handler)
+    data_hander = dataHandler("../output/", settings_handler, mcd_interface)
 
     # ===== Downloading data ======
-    
-    slon = 56
 
-    lat_step = settings_handler.get_setting("LAT_STEP")
-    lon_step = settings_handler.get_setting("LON_STEP")
     record_type = settings_handler.get_setting("RECORD_TYPE")
+    slon_step = settings_handler.get_setting("SLON_STEP")
 
-    for lat in range(LAT_MARGINS[0], LAT_MARGINS[1] + lat_step, lat_step):
 
-        # If currently checking the poles, we will assign the data of one point to the whole scale.
-        if lat == LAT_MARGINS[0] or lat == LAT_MARGINS[1]:
-
-            current_value = mcd_interface.do_query(slon, record_type, [lat, 180])
-
-            for lon in range(LON_MARGINS[0], LON_MARGINS[1] + lon_step, lon_step):
-                data = add_value_to_dict(data, lat, lon, current_value)
-
-                print(lat, lon, current_value)
-            continue
-
-        for lon in range(LON_MARGINS[0], LON_MARGINS[1] + lon_step, lon_step):
-            current_value = mcd_interface.do_query(slon, record_type, [lat, lon])
-
-            data = add_value_to_dict(data, lat, lon, current_value)
-
-            print(lat, lon, current_value)
-    
-    data_hander.write_database_slon_record(record_type, slon, data)
+    for slon in range(0, 360 + slon_step, slon_step):
+        data = data_hander.collect_data(slon, LAT_MARGINS, LON_MARGINS, record_type)
+        data_hander.write_database_slon_record(record_type, slon, data)
