e6ce3fc8df13d16031c0a7404f4a997321887f34
mattiapeiretti <mattia.peiretti@gmail.com>
2021-07-15 02:58:11 +0200

    💼 Created settingsHandler

---
+++ project/settingsHandler.py
@@ -0,0 +1,37 @@
+#----------------------------------------------------------------
+# Settings takes care of handling the settings of the whole
+# program.
+# Code written by MattiaPeiretti (c). https>//mattiapeiretti.com
+# Technical consultancy: Marco De Marco
+#----------------------------------------------------------------
+
+SETTINGS = {
+    "MCD_BASELINK": "http://www-mars.lmd.jussieu.fr/mcd_python/cgi-bin/mcdcgi.py",
+    "RECORDS_FILENAME_NUMBERS_PADDING": 3,
+    #"RECORD_TYPE": "tsurfmn",
+    "RECORD_TYPE": "tsurfmx",
+    "SLON_STEP": 90,
+    "LAT_STEP": 30,
+    "LON_STEP": 90,
+}
+
+class SettingsHandler():
+    __instance = None                                                                       # Instance shared varaible
+
+    #Singleton
+    def __new__(cls, *args, **kwargs):                                                      # Checking upon initialization that the class 
+        if not SettingsHandler.__instance:                                                  # hasn't already been initialized
+            SettingsHandler.__instance = object.__new__(cls)                                # if so returning the already
+        return SettingsHandler.__instance                                                   # initialized instance..
+    
+    def __init__(self):
+        self.settings = self.read_settings()
+
+    def get_setting(self, setting):
+        if self.settings[setting]:
+            return self.settings[setting]
+        else: 
+            return False
+
+    def read_settings(self):
+        return SETTINGS
