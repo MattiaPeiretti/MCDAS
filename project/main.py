d4b14dc25536c9ee9c5f6afd9def232feec807e7
mattiapeiretti <mattia.peiretti@gmail.com>
2021-07-16 22:27:14 +0200

    💼 Fixed arguments bug

--- project/main.py
+++ project/main.py
@@ -6,18 +6,20 @@ import sys
 
 if __name__ == "__main__":
 
-    if not sys.argv[1] and not sys.argv[2]:
+    if not sys.argv.count(1) and not sys.argv.count(2):
         LAT_MARGINS = [-90, 90] 
+    else:
+        LAT_MARGINS = [float(sys.argv[1]), float(sys.argv[2])] 
     
-    if not sys.argv[3] and not sys.argv[4]:
+    if not sys.argv.count(3) and not sys.argv.count(4):
         LON_MARGINS = [0, 360] 
-
-    LAT_MARGINS = [float(sys.argv[1]), float(sys.argv[2])] 
-    LON_MARGINS = [float(sys.argv[3]), float(sys.argv[4])] 
+    else:
+        LON_MARGINS = [float(sys.argv[3]), float(sys.argv[4])] 
+        
 
     settings_handler = SettingsHandler()
     mcd_interface = MCDInterface(settings_handler.get_setting("MCD_BASELINK"))
-    data_hander = dataHandler("../output/", settings_handler, mcd_interface)
+    data_hander = dataHandler("../output/", settings_handler, mcd_interface, LAT_MARGINS, LON_MARGINS)
 
     # ===== Downloading data ======
 
@@ -26,5 +28,5 @@ if __name__ == "__main__":
 
 
     for slon in np.arange(0, 360 + slon_step, slon_step):
-        data = data_hander.collect_data(slon, LAT_MARGINS, LON_MARGINS, record_type)
+        data = data_hander.collect_data(slon, record_type)
         data_hander.write_database_slon_record(record_type, slon, data)
