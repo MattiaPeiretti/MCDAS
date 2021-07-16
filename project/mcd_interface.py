aa3c3cd506859fe4433a84c2038de2c301a23828
mattiapeiretti <mattia.peiretti@gmail.com>
2021-07-16 23:44:19 +0200

    📦 Handling requests errors

--- project/mcd_interface.py
+++ project/mcd_interface.py
@@ -5,11 +5,14 @@
 # Technical consultancy: Marco De Marco
 #----------------------------------------------------------------
 
+from sys import excepthook
 import requests
 from bs4 import BeautifulSoup
 import re
+import time
 
 from settingsHandler import SettingsHandler
+import consoleGUI
 
 def find_number_in_string(string):
     numbers = []
@@ -22,6 +25,8 @@ def find_number_in_string(string):
 class MCDInterface():
     def __init__(self, base_link):
         self.base_link = base_link
+        self.settings_handler = SettingsHandler()
+        self.gui = consoleGUI.GUI("Error")
 
     def do_query(self, slon, variable, coordinates):
         lat = coordinates[0]
@@ -33,10 +38,24 @@ class MCDInterface():
         #     alt = 0
         # else:
         #     alt = coordinates[2]
+        request_URI = f"{self.base_link}?ls={slon}&latitude={lat}&longitude={lon}&altitude={alt}&zkey=3&isfixedlt=on&dust=1&hrkey=1&zonmean=on&var1={variable}&var2=none&var3=none&var4=none&dpi=80&islog=off&colorm=jet&minval=&maxval=&proj=cyl&plat=&plon=&trans=&iswind=off&latpoint=&lonpoint="
+        
+        try:
+            response = requests.get(request_URI)
+            if not response.status_code == 200:
+                raise Exception()
+        except:
+            for try_nr in range(0, self.settings_handler.get_setting("REQUEST_RETRY_AMOUNT")):
+                self.gui.display_error("Request Error Occured", f"Trying to connect again: try {try_nr} of {self.settings_handler.get_setting('REQUEST_RETRY_AMOUNT')}")
+                time.sleep(self.settings_handler.get_setting("REQUEST_RETRY_WAITTIME"))
+                try:
+                    response = requests.get(request_URI)
+                    break
+                except:
+                    pass
+            return self.settings_handler.get_setting("REQUEST_ERROR_CHARACTER")
 
-
-        raw_html = requests.get(f"{self.base_link}?ls={slon}&latitude={lat}&longitude={lon}&altitude={alt}&zkey=3&isfixedlt=on&dust=1&hrkey=1&zonmean=on&var1={variable}&var2=none&var3=none&var4=none&dpi=80&islog=off&colorm=jet&minval=&maxval=&proj=cyl&plat=&plon=&trans=&iswind=off&latpoint=&lonpoint=")
-        soup = BeautifulSoup(raw_html.text, "html.parser")
+        soup = BeautifulSoup(response.text, "html.parser")
         
         value = soup.find("li").text
 
