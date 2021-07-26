0dfda5b5f2e93d95d21fa82027f6a8a26a2bc701
Mattia Peiretti <mattia.peiretti@gmail.com>
2021-07-26 02:56:31 +0200

    📦 Created a new custom GUI lib for the future gui of the project

---
+++ project/GUI/MCDAS_GUI_lib.py
@@ -0,0 +1,89 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from functools import partial
+
+from project.GUI.stylesheet import STYLES
+
+
+class root(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Mars Climate Database Augmented Software")
+
+        self.stacked_widget = QStackedWidget()
+        self.stacked_widget.setObjectName("pageBody")
+
+        self.header = QHBoxLayout()
+
+        self.page_title_label = QLabel("Loading...")
+        self.page_title_label.setObjectName("PageHeaderLabel")
+        self.header.addWidget(self.page_title_label)
+
+        self.pages_register = {}
+        self.navbar_buttons = []
+        self.page_titles = {}
+
+        self.setStyleSheet(STYLES)
+
+    def update_header(self, index):
+        self.page_title_label.setText(self.page_titles[index])
+        for count, button in enumerate(self.navbar_buttons):
+            if not count == index:
+                button.setStyleSheet("color: black;")
+            else:
+                button.setStyleSheet("color: #6495ED;")
+
+    def insert_page(self, widget, name):
+        index = len(self.pages_register)
+        self.stacked_widget.insertWidget(index, widget)
+        self.pages_register[index] = name
+        if hasattr(widget, "name"):
+            self.page_titles[index] = widget.name
+        else:
+            self.page_titles[index] = "Untitled Page"
+        self.update_header(self.stacked_widget.currentIndex())
+
+    def generate_navbar(self):
+        for index, button_name in self.pages_register.items():
+            button = QPushButton(button_name)
+            button.setObjectName("NavbarButton")
+            button.clicked.connect(partial(self.stacked_widget.setCurrentIndex, index))
+            button.setCursor(QCursor(Qt.PointingHandCursor))
+            self.navbar_buttons.append(button)
+            self.header.addWidget(button)
+
+    def initialize(self, displayDebugBorders=False):
+        if displayDebugBorders:
+            self.setStyleSheet("QWidget {border: 1px solid red;}")
+
+        self.body = QVBoxLayout()
+
+        self.generate_navbar()
+
+        self.body.addLayout(self.header)
+
+        self.body.addWidget(self.stacked_widget)
+        self.stacked_widget.currentChanged.connect(self.update_header)
+
+        self.update_header(0)
+        self.setLayout(self.body)
+
+
+class CustomPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.name = "Untitled Page"
+        self.page_body = QVBoxLayout()
+        self.page_body.setAlignment(Qt.AlignTop)
+
+        self.build()
+
+        self.page_body.addStretch()
+        self.setLayout(self.page_body)
+
+    def build(self):
+        return
+
+    def set_page_title(self, title):
+        self.name = title
---
+++ project/GUI/stylesheet.py
@@ -0,0 +1,22 @@
+STYLES = """
+
+#NavbarButton {
+    border: none;
+    color: grey;
+}
+
+#PageHeaderLabel {
+    font-size: 16px;
+    position: realtive;
+    top: 10px;
+    left: 20px;
+    padding-right: 20px;
+    min-width: 200px;
+}
+
+#pageBody {
+    border: 1px solid grey;
+    background-color: white;
+}
+
+"""
